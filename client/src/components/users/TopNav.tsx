79658e30f63f81ad779ed4345a855c8076114aba
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-13 16:31:44 +0100

    updated top nav

--- client/src/components/users/TopNav.tsx
+++ client/src/components/users/TopNav.tsx
@@ -7,28 +7,26 @@ const TopNav = () => {
     <div className='top-bar container-fluid'>
       <div className="container h-100">
         <div className="h-100 row justify-content-center align-items-center">
-          <div className="col-md-4 col sm-6 log-icons d-flex align-items-center">
+          <div className="col-md-4 col sm-6 log-icons d-flex align-items-center justify-content-center">
             <p className="py-2 my-auto"><i className="fa fa-phone"></i>Call Taxi : 12(00) 123 1234</p>
           </div>
-          <div className="col-md-4 col-sm-6 logo">
+          <div className="col-md-4 col-sm-6 logo d-flex justify-content-center">
             <NavLink className="navbar-brand" to="/"> <i className="fas fa-taxi"></i> Taxi Cab</NavLink>
           </div>
-          <div className="col-md-4">
-            <div className="row align-items-center">
-              <div className="col-6">
-                <span className="">
-                  <NavLink className='nav-link' to="/sign-in">
-                    <i className="fas fa-lock"></i> Sign In
-                  </NavLink>
-                </span>
-              </div>
-              <div className="col-6">
-                <span className="">
-                  <NavLink className='nav-link' to="/signup" >
-                    <i className="fas fa-user"></i> Register
-                  </NavLink>
-                </span>
-              </div>
+          <div className="col-md-4 top-forms">
+            <div className="wrapper">
+              <span className="">
+                <NavLink className='nav-link' to="/sign-in">
+                  <i className="fas fa-lock me-2"></i>Sign in
+                </NavLink>
+              </span>
+            </div>
+            <div className="wrapper">
+              <span className="">
+                <NavLink className='nav-link' to="/signup" >
+                  <i className="fas fa-user me-2"></i>Register
+                </NavLink>
+              </span>
             </div>
           </div>
         </div>
