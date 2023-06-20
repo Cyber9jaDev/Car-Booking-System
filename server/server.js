b28a6db4c95d5fc2a6e4580aef26fec9d9f9a395
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-20 21:14:42 +0100

    updated signup API

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './db/connection.js';
+import UserRouter from './routes/users/UserRoute.js';
 
 const app = express();
 dotenv.config();
@@ -11,14 +12,17 @@ app.use(express.json());
 app.use(cors());
 
 
+app.use('/api/v1', UserRouter);
+
+
 const startApp = async() => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(process.env.PORT, () => console.log('LISTENING ON PORT', process.env.PORT))
+    app.listen(process.env.PORT, () => console.log('LISTENING ON PORT', process.env.PORT));
   } catch (error) {
     console.log(error);
   }
 }
 
 
-startApp()
+startApp();
