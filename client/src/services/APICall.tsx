40db838e550bf082fda90195842f4a900285ab4c
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-23 23:57:41 +0100

    minor update

--- client/src/services/APICall.tsx
+++ client/src/services/APICall.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Toast } from '../utilities/utils';
 // import { payloadType } from './';
 
 export default async function APICall(url: string, method: string, data: unknown){
@@ -10,9 +11,28 @@ export default async function APICall(url: string, method: string, data: unknown
     axios.defaults.headers.common['Content-Type'] = 'application/json';
   }
 
+  const baseUrl = 'http://localhost:5000';
 
+  if(!url.startsWith('/')){
+    url = '/' + url;
+  }
 
-  const response = await axios({ method, url, data });
+  const response = await axios({ 
+    method, 
+    url: baseUrl + url.trim(), 
+    data 
+  });
+
+  if(response){
+    if(!response.status || response.status === 0){ // No response
+      Toast('error', 'Sorry, it seems you are not connected to the internet. Please check you network connection and try again');
+      return null;
+    }
+  }
+
+  else{
+    return 
+  }
 
   return !response ? null : response;
 }
