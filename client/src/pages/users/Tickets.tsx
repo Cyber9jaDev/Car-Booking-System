c6a7daef6abeab1128d532b3b3be2fdf70d94a4b
Cyber9ja <ayodejioladapo96@gmail.com>
2023-07-05 23:44:22 +0100

    update ticket

--- client/src/pages/users/Tickets.tsx
+++ client/src/pages/users/Tickets.tsx
@@ -1,9 +1,11 @@
 import BookingTicket from '../../components/users/BookingTicket';
 import BusSelection from '../../components/users/BusSelection';
+import SeatModal from '../../components/users/SeatModal';
 import '../../sass/tickets.scss';
 const Tickets = () => {
   return (
-    <main id='tickets'>
+    <main id='tickets' className='position-relative'>
+      <SeatModal />
       <BookingTicket />
       <BusSelection />
     </main>
