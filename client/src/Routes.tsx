40349134ca7355d498d72a48ba7d847246960fa7
Cyber9ja <ayodejioladapo96@gmail.com>
2023-07-16 18:11:44 +0100

    minor update

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -8,7 +8,6 @@ import Services from "./pages/users/Services";
 import Contact from "./pages/users/Contact";
 import Home from "./pages/admin/Home";
 import Bookings from "./pages/users/Bookings";
-import { BookingContextProvider } from "./contexts/BookingContext";
 import PassengerDetails from "./pages/users/PassengerDetails";
 import PaymentModal from "./components/users/modals/PaymentModal";
 import Thanks from "./pages/users/Thanks";
@@ -16,28 +15,23 @@ import Thanks from "./pages/users/Thanks";
 
 const RouterLinks = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<SharedLayout/>}>
-            <Route index element={ <Homepage/> } />
-          </Route>
-          <Route path="/register" element={<Signup/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/contact" element = {<Contact/>} />
-          <Route path="/passenger-details" element = {<PassengerDetails/>} />
-          <Route path="/modal" element = {<PaymentModal/>} />
-          <Route path="/paystack/callback" element = {<Thanks/>} />
-          <Route path="/booking" element = {
-            <BookingContextProvider>
-              <Bookings/>
-            </BookingContextProvider>} />
-          <Route path="/admin/home" element={ <Home/>} /> 
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout/>}>
+          <Route index element={ <Homepage/> } />
+        </Route>
+        <Route path="/register" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/contact" element = {<Contact/>} />
+        <Route path="/passenger-details" element = {<PassengerDetails/>} />
+        <Route path="/modal" element = {<PaymentModal/>} />
+        <Route path="/paystack/callback" element = {<Thanks/>} />
+        <Route path="/booking" element = { <Bookings/> } />
+        <Route path="/admin/home" element={ <Home/>} /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/users/PassengerDetails.tsx
+++ client/src/pages/users/PassengerDetails.tsx
@@ -1,10 +1,37 @@
 import { Link } from 'react-router-dom';
 import '../../sass/passenger-details.scss';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../../contexts/UserContext';
+// import Modal from '../../components/modal/Modal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// import Modal from '@mui/material/Modal';
+import { Modal } from '@mui/material';
 
 const PassengerDetails = () => {
   const {currentUser, setNextOfKin} = useContext(UserContext);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+      setOpen(false);
+  };
+
+  const handleOpen = () => {
+      setOpen(true);
+  };
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <main id='passenger-details' className='py-5'>
@@ -90,8 +117,28 @@ const PassengerDetails = () => {
 
         </div>
       </div>
+
+      {/* <Modal>test</Modal> */}
+      <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
     </main>
   )
 }
 
-export default PassengerDetails
+export default PassengerDetails;
