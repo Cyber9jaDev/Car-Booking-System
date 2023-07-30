23a315f812783b6beddc6c01b540a947f6ad604d
Cyber9ja <ayodejioladapo96@gmail.com>
2023-07-30 15:25:57 +0100

    removed react strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+    <App />
 )
+
+
+  // <React.StrictMode>
+  //     <App />
+  // </React.StrictMode>,
