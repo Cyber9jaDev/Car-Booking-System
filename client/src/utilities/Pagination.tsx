28c6f0042bb1c0215ac96ccde9e31c3e5cbafb99
Cyber9ja <ayodejioladapo96@gmail.com>
2023-08-15 21:19:48 +0100

    added pagination component

--- client/src/utilities/Pagination.tsx
+++ client/src/utilities/Pagination.tsx
@@ -9,7 +9,7 @@ const Pagination = () => {
       <ul className='p-0 d-flex align-items-center'>
         <li onClick={() => setPage(page => page - 1)}  className="d-inline-block me-2 border border-primary rounded px-3 py-2 text-white"><i className="fa-solid fa-angles-left"></i></li>
         <li onClick={() => setPage(page => page - 1)}  className="d-inline-block me-5 border border-primary rounded px-3 py-2 text-white"><i className="fa-solid fa-angle-left"></i></li>
-        <li className='d-inline-block'>Page<span className='mx-1'>{page}</span> of <span className='mx-1'>{totalPages}</span></li>
+        <li className='d-inline-block page'>Page<span className='mx-1'>{page}</span> of <span className='mx-1'>{totalPages}</span></li>
         <li onClick={() => setPage(page => page + 1)} className="d-inline-block ms-5 border border-primary rounded px-3 py-2 text-white"><i className="fa-solid fa-angle-right"></i></li>
         <li onClick={() => setPage(page => page + 1)}  className="d-inline-block ms-2 border border-primary rounded px-3 py-2 text-white"><i className="fa-solid fa-angles-right"></i></li>
       </ul>
